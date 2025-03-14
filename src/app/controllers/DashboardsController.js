c427df6b0a2ec17cb48d67ef06a70a4aeee273b1
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-03-14 16:33:07 -0300

    feat(dashboard): retrieve all orders from the database

--- src/app/controllers/DashboardsController.js
+++ src/app/controllers/DashboardsController.js
@@ -3,28 +3,14 @@ import Order from "../models/Order";
 class DashboardsController {
   async index(req, res) {
     try {
-      // Pegar o token do cabeçalho da requisição
-      const authHeader = req.headers.authorization;
-
-      if (!authHeader) {
-        return res.status(401).json({
-          error: true,
-          message: "Token não fornecido. Faça login novamente.",
-        });
-      }
-
-      // Buscar os pedidos no banco de dados
+      // Buscar todos os pedidos no banco
       const orders = await Order.find();
 
-      return res.status(200).json({
-        success: true,
-        message: "Pedidos recuperados com sucesso.",
-        data: orders,
-      });
+      
     } catch (error) {
       return res.status(500).json({
         error: true,
-        message: "Erro ao buscar os pedidos. Tente novamente mais tarde.",
+        message: "Erro ao calcular a dashboard. Tente novamente mais tarde."
       });
     }
   }
