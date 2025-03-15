c27a4df6162672df39470cbf805c4f8897c0b607
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-03-15 09:18:27 -0300

    feat(Login): Start login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login/Login";
+
 function App() {
 
   return (
-    <>
-      hello word
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/Login/Login.jsx
@@ -0,0 +1,10 @@
+import './Login.css'
+
+function Login (){
+    return(
+        <>
+          Login
+        </>
+    )
+}
+export default Login
